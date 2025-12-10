a7c311341fc3fa93e06bf43d6c87ddc5cc9a46f2
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-12-10 13:47:21 -0300

    Código da Situação aprendizagem de internet das coisas
    Esse código consiste em um sistema de nível e controle de água, conforme a bomba enche, irá medir o nível de água e quando chegar no limite determinado, irá parar de encher, ativar o buzzer, e aparecer a mensagem no LCD

---
+++ Codigo_VNV.cpp
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// --- LCD ---
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// --- PINOS ---
+const int trigPin = 3;
+const int echoPin = 2;
+const int buzzerPin = 7;
+const int bombaPin = 9;  // <-- bomba aqui
+
+// --- VARIÁVEIS ---
+long duracao;
+int distancia;
+int distanciaCritica = 5; // cm
+
+void setup() {
+  // LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("  Iniciando...");
+  delay(1500);
+  lcd.clear();
+
+  // Sensor e atuadores
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(bombaPin, OUTPUT);
+
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(bombaPin, LOW);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Sistema VNV OK");
+  delay(1500);
+  lcd.clear();
+}
+
+void loop() {
+  // --- MEDIÇÃO ULTRASSÔNICA ---
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duracao = pulseIn(echoPin, HIGH);
+  distancia = duracao * 0.034 / 2;
+
+  // --- EXIBE DISTÂNCIA ---
+  lcd.setCursor(0, 0);
+  lcd.print("Distancia: ");
+  lcd.print(distancia);
+  lcd.print("cm   ");
+
+  // ---------------------------
+  //      CONTROLE DA BOMBA
+  // ---------------------------
+
+  if (distancia > 0 && distancia <= distanciaCritica) {
+    // *** NÍVEL CRÍTICO - PARAR BOMBA ***
+    digitalWrite(bombaPin, LOW); // desliga bomba
+    tone(buzzerPin, 1000);
+
+    lcd.setCursor(0, 1);
+    lcd.print("Nivel Alto! ");
+  } 
+  else if (distancia > distanciaCritica) {
+    // *** ENCHENDO ***
+    digitalWrite(bombaPin, HIGH); // liga bomba
+    noTone(buzzerPin);
+
+    lcd.setCursor(0, 1);
+    lcd.print("Enchendo...     ");
+  } 
+  else {
+    lcd.setCursor(0, 1);
+    lcd.print("Sem leitura     ");
+  }
+
+  delay(500);
+}
